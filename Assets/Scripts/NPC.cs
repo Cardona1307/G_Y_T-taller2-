b268a8a647fd7eefca14db1481d6d16103c25175
DevilishhSmile <111027182+DevilishhSmile@users.noreply.github.com>
2024-12-04 02:47:52 -0500

    Added Dialogos to NPC Robot Office

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,66 +1,66 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class RecogerJuguetesMision : MonoBehaviour, IMision
-{
-    public string[] objetosRequeridos = { "Juguete1", "Juguete2", "Juguete3", "Juguete4", "Juguete5" };
-    private Inventario inventarioJugador;
-    private bool misionCompletada = false;
+//public class RecogerJuguetesMision : MonoBehaviour, IMision
+//{
+//    public string[] objetosRequeridos = { "Juguete1", "Juguete2", "Juguete3", "Juguete4", "Juguete5" };
+//    private Inventario inventarioJugador;
+//    private bool misionCompletada = false;
 
-    public string ObtenerDescripcion()
-    {
-        return "Recoge todos los juguetes y entrégalos al NPC.";
-    }
+//    public string ObtenerDescripcion()
+//    {
+//        return "Recoge todos los juguetes y entrégalos al NPC.";
+//    }
 
-    public bool EstaCompletada()
-    {
-        return misionCompletada;
-    }
+//    public bool EstaCompletada()
+//    {
+//        return misionCompletada;
+//    }
 
-    public void CompletarMision()
-    {
-        misionCompletada = true;
-        Debug.Log("¡Has completado la misión de recoger juguetes!");
-    }
+//    public void CompletarMision()
+//    {
+//        misionCompletada = true;
+//        Debug.Log("¡Has completado la misión de recoger juguetes!");
+//    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            inventarioJugador = other.GetComponent<Inventario>();
-        }
-    }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            inventarioJugador = other.GetComponent<Inventario>();
+//        }
+//    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            inventarioJugador = null;
-        }
-    }
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            inventarioJugador = null;
+//        }
+//    }
 
-    public void IntentarCompletarMision()
-    {
-        if (inventarioJugador != null && TieneObjetosSuficientes())
-        {
-            inventarioJugador.LimpiarInventario();
-            inventarioJugador.AgregarObjeto("piezaRecompensa");
-            CompletarMision();
-        }
-        else
-        {
-            Debug.Log("No tienes todos los juguetes necesarios.");
-        }
-    }
+//    public void IntentarCompletarMision()
+//    {
+//        if (inventarioJugador != null && TieneObjetosSuficientes())
+//        {
+//            inventarioJugador.LimpiarInventario();
+//            inventarioJugador.AgregarObjeto("piezaRecompensa");
+//            CompletarMision();
+//        }
+//        else
+//        {
+//            Debug.Log("No tienes todos los juguetes necesarios.");
+//        }
+//    }
 
-    private bool TieneObjetosSuficientes()
-    {
-        foreach (string objeto in objetosRequeridos)
-        {
-            if (!inventarioJugador.TieneObjeto(objeto))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+//    private bool TieneObjetosSuficientes()
+//    {
+//        foreach (string objeto in objetosRequeridos)
+//        {
+//            if (!inventarioJugador.TieneObjeto(objeto))
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
